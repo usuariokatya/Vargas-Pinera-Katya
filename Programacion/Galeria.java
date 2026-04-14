dbc3d4f0839399d3002957fd3a255557cb02e2a9
Katya <katya.thais20@gmail.com>
2026-04-14 09:21:02 +0200

    Galeria.java actualizada y mejorada

--- Programacion/Galeria.java
+++ Programacion/Galeria.java
@@ -6,14 +6,12 @@ public class Galeria {
     private String titulo;
     private int idEvento;
 
-    // Constructor
     public Galeria(int id, String titulo, int idEvento) {
         this.id = id;
         this.titulo = titulo;
         this.idEvento = idEvento;
     }
 
-    // Getters
     public int getId() {
         return id;
     }
@@ -26,25 +24,8 @@ public class Galeria {
         return idEvento;
     }
 
-    // Setters
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public void setIdEvento(int idEvento) {
-        this.idEvento = idEvento;
-    }
-
-    // toString
+    @Override
     public String toString() {
-        return "Galeria{" +
-                "id=" + id +
-                ", titulo='" + titulo + '\'' +
-                ", idEvento=" + idEvento +
-                '}';
+        return "Galería #" + id + ": " + titulo;
     }
 }
